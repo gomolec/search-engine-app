fdf3f98d741d271b2848618515b3de6c6628edbd
gomolec <gomolecs@gmail.com>
2022-05-29 15:55:49 +0200

    Project basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'search-engine', 
+    loadChildren: () => import('./search-engine/search-engine.module').then(m => m.SearchEngineModule) 
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
